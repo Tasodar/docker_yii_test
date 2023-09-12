3fea38aa714f8097af05ca9a3b8a202583cf89d8
prizrak <tasodar@gmail.com>
2023-09-12 18:11:01 +0300

    start point

---
+++ www/.bash_history
@@ -0,0 +1,18 @@
+ls
+exit
+composer -v
+pwd
+exit
+pwd
+composer create-project --prefer-dist yiisoft/yii2-app-basic app
+mc
+ls
+exit
+ls
+cd app/
+init
+yii init
+ls
+yii.bat init
+ls -la
+exit
---
+++ www/app/views/layouts/custom.php
@@ -0,0 +1,22 @@
+<?php
+
+    use app\assets\AppAsset;
+
+    AppAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
